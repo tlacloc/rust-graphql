98ab6684d60fa42e5c6b40bca1ae2228b8fec44a
Erick Casanova <desneruda@gmail.com>
2022-07-12 11:26:27 -0500

    update graph schema to use diesel schema

--- src/graphql_schema.rs
+++ src/graphql_schema.rs
@@ -1,8 +1,51 @@
+extern crate dotenv;
+
+use std::env;
+
+use diesel::pg::PgConnection;
+use diesel::prelude::*;
+use dotenv::dotenv;
+
 use juniper::{EmptyMutation, RootNode};
 
+use crate::schema::members;
+
+pub struct QueryRoot;
+
+fn establish_connection() -> PgConnection {
+  dotenv().ok();
+
+  let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+  PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
+}
+
+#[juniper::object]
+impl QueryRoot {
+  fn members(&self) -> Vec<Member> {
+    use crate::schema::members::dsl::*;
+    let connection = establish_connection();
+    members
+      .limit(100)
+      .load::<Member>(&connection)
+      .expect("Error loading members")
+  }
+
+  fn teams() -> Vec<Team> {
+    use crate::schema::teams::dsl::*;
+    let connection = establish_connection();
+    teams
+      .limit(100)
+      .load::<Team>(&connection)
+      .expect("Error loading teams")
+  }
+}
+
+#[derive(Queryable)]
 struct Member {
-  id: i32,
-  name: String,
+  pub id: i32,
+  pub name: String,
+  pub knockouts: i32,
+  pub team_id: i32,
 }
 
 #[juniper::object(description = "A member of a team")]
@@ -14,24 +57,41 @@ impl Member {
   pub fn name(&self) -> &str {
     self.name.as_str()
   }
+
+  pub fn knockouts(&self) -> i32 {
+    self.knockouts
+  }
+
+  pub fn team_id(&self) -> i32 {
+    self.team_id
+  }
 }
 
-pub struct QueryRoot;
+#[derive(Queryable)]
+struct Team {
+  pub id: i32,
+  pub name: String,
+}
 
-// hardcoding some data here, but in a real world application, this data would be taken from a database
-#[juniper::object]
-impl QueryRoot {
-  fn members() -> Vec<Member> {
-    vec![
-      Member {
-        id: 1,
-        name: "Link".to_owned(),
-      },
-      Member {
-        id: 2,
-        name: "Mario".to_owned(),
-      },
-    ]
+#[juniper::object(description = "A team of members")]
+impl Team {
+  pub fn id(&self) -> i32 {
+    self.id
+  }
+
+  pub fn name(&self) -> &str {
+    self.name.as_str()
+  }
+
+  pub fn team(&self) -> Vec<Member> {
+    use crate::schema::members::dsl::*;
+    let connection = establish_connection();
+
+    members
+      .limit(100)
+      .filter(team_id.eq(self.id))
+      .load::<Member>(&connection)
+      .expect("Error loading members")
   }
 }
 
